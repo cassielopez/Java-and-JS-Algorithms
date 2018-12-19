82f523763bd42255db8edc133b0da69251c7de32
cassielopez <cat5xmbi@gmail.com>
2018-12-19 17:21:35 -0600

    Create manipulate-arrays-with-pop.js
    "pop" a value off of the end of an array and return that value.

---
+++ manipulate-arrays-with-pop.js
@@ -0,0 +1,12 @@
+// Example ourArray
+var ourArray = [1,2,3];
+var removedFromOurArray = ourArray.pop(); 
+// removedFromOurArray now equals 3, and ourArray now equals [1,2]
+
+// Setup myArray
+var myArray = [["John", 23], ["cat", 2]];
+
+// pop function 
+var removedFromMyArray = myArray.pop();
+
+
