9d755832d1fa4b7d115f10906f02e3ad6b98cd17
cassielopez <cat5xmbi@gmail.com>
2018-12-19 17:32:24 -0600

    Create shopping-list.js
    Create a shopping list in the variable myList.

---
+++ shopping-list.js
@@ -0,0 +1,15 @@
+//There should be at least 5 sub-arrays in the list.
+
+var myList = [
+["Chocolate Bar", 15],
+
+["Candy Cane", 17],
+
+["Halved Almonds", 13],
+
+["Marshmallow Creme", 12],
+
+["Cinnamon Stick", 23]
+
+];
+
