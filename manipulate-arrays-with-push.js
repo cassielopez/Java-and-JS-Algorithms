88082d5865b963cc0b58f7aad9146b5bd14eb534
cassielopez <cat5xmbi@gmail.com>
2018-12-19 17:13:27 -0600

    Create manipulate-arrays-with-push.js

---
+++ manipulate-arrays-with-push.js
@@ -0,0 +1,12 @@
+// Array Example 1
+var ourArray = ["Stimpson", "J", "cat"];
+exArray.push(["happy", "joy"]); 
+// exArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Array Example 2
+var myArray = [["John", 23], ["cat", 2]];
+
+// Array Push
+myArray.push(["dog", 3]);
+
+// myArray now equals [["John", 23], ["cat", 2],["dog", 3]]
