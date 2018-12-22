e2de8028ae73b9131cf022ce1aa750873862d3fa
cassielopez <cat5xmbi@gmail.com>
2018-12-22 15:25:02 -0600

    Create global-vs-local-scope-in-functions.java
    Add a local variable to myOutfit function to override the value of outerWear with "sweater".

---
+++ global-vs-local-scope-in-functions.java
@@ -0,0 +1,12 @@
+//Add a local variable to myOutfit function to override the value of outerWear with "sweater".
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  var outerWear = "sweater";
+  
+  return outerWear;
+}
+
+myOutfit();
