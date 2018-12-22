093b0734142c5076d7e43c4f2e411d6fc119dda5
cassielopez <cat5xmbi@gmail.com>
2018-12-22 15:42:26 -0600

    Create return-a-value-from-a-function-with-return.js
    Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value. See the last line in the editor for an example of how you can test your timesFive function.

---
+++ return-a-value-from-a-function-with-return.js
@@ -0,0 +1,9 @@
+//Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value. See the last line in the editor 
+//for an example of how you can test your timesFive function.
+
+function timesFive(num){
+return num * 5;
+}
+
+var answer = timesFive(5);
+console.log(minusSeven(10));
