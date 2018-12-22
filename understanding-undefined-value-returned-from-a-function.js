dd101348c2d34495223aa7b3136a2493033bf1ed
cassielopez <cat5xmbi@gmail.com>
2018-12-22 15:51:45 -0600

    Create understanding-undefined-value-returned-from-a-function.js
    Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.

---
+++ understanding-undefined-value-returned-from-a-function.js
@@ -0,0 +1,7 @@
+//Create a function addFive without any arguments. This function adds 5 to the sum variable, but its returned value is undefined.
+
+function addFive(){
+  sum = sum + 5;
+}
+
+var returnedValue = addFive();
